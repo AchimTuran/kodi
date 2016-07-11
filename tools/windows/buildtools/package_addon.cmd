760b81f68eca970609247975342b4fd152ed462a
AchimTuran <mail@achim-turan.de>
2016-07-11 18:49:21 +0200

    [windows][buildtools] Add option to install add-ons into a seperate path

--- tools/windows/buildtools/package_addon.cmd
+++ tools/windows/buildtools/package_addon.cmd
@@ -6,6 +6,12 @@ if "%kodi_src%" EQU "" (
   set kodi_src=%1%
 )
 
+if not "%2%" EQU "" (
+set addon_install=%2%
+) else (
+set addon_install=%kodi_src%\addons
+)
+
 set addon_folder=%cd%
 
 echo using %kodi_src% to package add-on: %addon_id%
@@ -20,8 +26,9 @@ cd %addon_folder%\package\build
 :: use / delimiter for paths to make CMake happy
 set cmake_addon_folder=%addon_folder:\=/%
 set cmake_kodi_src=%kodi_src:\=/%
+set cmake_addon_install=%addon_install:\=/%
 
-cmake -G "NMake Makefiles" -DADDONS_TO_BUILD="%addon_id%" -DADDON_SRC_PREFIX="%cmake_addon_folder%/.." -DADDONS_DEFINITION_DIR="%cmake_addon_folder%/../../repo-binary-addons" -DCMAKE_INSTALL_PREFIX="%cmake_kodi_src%/addons" -DCMAKE_VERBOSE_MAKEFILE=ON -DPACKAGE_ZIP=ON -DPACKAGE_DIR=%cmake_addon_folder%/package -DCMAKE_BUILD_TYPE=Release "%cmake_kodi_src%/project/cmake/addons"
+cmake -G "NMake Makefiles" -DADDONS_TO_BUILD="%addon_id%" -DADDON_SRC_PREFIX="%cmake_addon_folder%/.." -DADDONS_DEFINITION_DIR="%cmake_addon_folder%/../repo-binary-addons" -DCMAKE_INSTALL_PREFIX="%cmake_addon_install%" -DCMAKE_VERBOSE_MAKEFILE=ON -DPACKAGE_ZIP=ON -DPACKAGE_DIR=%cmake_addon_folder%/package -DCMAKE_BUILD_TYPE=Release "%cmake_kodi_src%/project/cmake/addons"
 
 %cmake_kodi_src%/tools/windows/buildtools/jom.exe -j8 %addon_id%
 %cmake_kodi_src%/tools/windows/buildtools/jom.exe -j8 package-%addon_id%
