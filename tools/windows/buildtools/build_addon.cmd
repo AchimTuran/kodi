8f0bd608aaf074e3fb27398e4b191ba404fc1b39
AchimTuran <mail@achim-turan.de>
2016-07-11 18:49:21 +0200

    [windows][buildtools] Add option to install add-ons into a seperate path

--- tools/windows/buildtools/build_addon.cmd
+++ tools/windows/buildtools/build_addon.cmd
@@ -6,6 +6,12 @@ if "%kodi_src%" EQU "" (
   set kodi_src=%1%
 )
 
+if not "%2%" EQU "" (
+set addon_install=%2%
+) else (
+set addon_install=%kodi_src%\addons
+)
+
 set addon_folder=%cd%
 
 echo using %kodi_src% to build workspace for add-on: %addon_id%
@@ -20,8 +26,9 @@ cd %addon_folder%\build
 :: use / delimiter for paths to make CMake happy
 set cmake_addon_folder=%addon_folder:\=/%
 set cmake_kodi_src=%kodi_src:\=/%
+set cmake_addon_install=%addon_install:\=/%
 
-cmake -G "Visual Studio 14 2015" -DADDONS_TO_BUILD="%addon_id%" -DADDON_SRC_PREFIX="%cmake_addon_folder%\.." -DADDONS_DEFINITION_DIR="%cmake_addon_folder%\..\repo-binary-addons" -DCMAKE_INSTALL_PREFIX="%cmake_kodi_src%/addons" -DCMAKE_VERBOSE_MAKEFILE=ON -DPACKAGE_ZIP=ON "%cmake_kodi_src%\project\cmake\addons"
+cmake -G "Visual Studio 14 2015" -DADDONS_TO_BUILD="%addon_id%" -DADDON_SRC_PREFIX="%cmake_addon_folder%/.." -DADDONS_DEFINITION_DIR="%cmake_addon_folder%/../repo-binary-addons" -DCMAKE_INSTALL_PREFIX="%cmake_addon_install%" -DCMAKE_VERBOSE_MAKEFILE=ON -DPACKAGE_ZIP=ON "%cmake_kodi_src%/project/cmake/addons"
 
 msbuild package-%addon_id%.vcxproj /p:Configuration=Debug
 
