01896345ef63b9ff4946324cd3614d81da5e58ba
ronie <ronie@users.noreply.github.com>
2016-09-09 22:28:58 +0200

    require click action to activate slider controls

--- xbmc/guilib/GUISliderControl.cpp
+++ xbmc/guilib/GUISliderControl.cpp
@@ -60,7 +60,6 @@ CGUISliderControl::CGUISliderControl(int parentID, int controlID, float posX, fl
   m_dragging = false;
   m_action = NULL;
   m_active = false;
-  m_keepactive = false;
 }
 
 CGUISliderControl::~CGUISliderControl(void)
@@ -247,12 +246,6 @@ bool CGUISliderControl::OnAction(const CAction &action)
     break;
   }
   return CGUIControl::OnAction(action);
-}
-
-void CGUISliderControl::OnUnFocus()
-{
-  if (!m_keepactive)
-    m_active = false;
 }
 
 void CGUISliderControl::Move(int iNumSteps)
@@ -503,17 +496,6 @@ void CGUISliderControl::SetFloatRange(float fStart, float fEnd)
     m_floatValues[0] = m_fStart = fStart;
     m_floatValues[1] = m_fEnd = fEnd;
   }
-}
-
-void CGUISliderControl::SetActive()
-{
-  m_active = true;
-}
-
-
-void CGUISliderControl::KeepActive()
-{
-  m_keepactive = true;
 }
 
 void CGUISliderControl::FreeResources(bool immediately)
