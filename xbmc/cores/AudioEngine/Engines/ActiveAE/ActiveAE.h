e7c1c09297e002e96e5bfee9fcc194cc89c11167
AchimTuran <mail@achim-turan.de>
2017-07-30 08:53:30 +0200

    Revert "Revert "[AE] drop GetAudioDSP method""
    This reverts commit 1bd1ab31e329568dbacca3e49f850474c7a830c2.

--- xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.h
+++ xbmc/cores/AudioEngine/Engines/ActiveAE/ActiveAE.h
@@ -265,8 +265,6 @@ public:
   void OnResetDisplay() override;
   void OnAppFocusChange(bool focus) override;
 
-  virtual IAEAudioDSP& GetAudioDSP() { return m_audioDSP; }
-
 protected:
   void PlaySound(CActiveAESound *sound);
   static uint8_t **AllocSoundSample(SampleConfig &config, int &samples, int &bytes_per_sample, int &planes, int &linesize);
--- xbmc/cores/AudioEngine/Interfaces/AE.h
+++ xbmc/cores/AudioEngine/Interfaces/AE.h
@@ -260,6 +260,4 @@ public:
    * @return Returns true on success, else false.
    */
   virtual bool GetCurrentSinkFormat(AEAudioFormat &SinkFormat) { return false; }
-
-  virtual IAEAudioDSP& GetAudioDSP() = 0;
 };
